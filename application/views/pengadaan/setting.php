2430bb52b2562fdc94e1afe7d900e157081ffb30
deded89 <dedisetiawan34@gmail.com>
2023-01-06 08:15:13 +0800

    tambah tahun setting

--- application/views/pengadaan/setting.php
+++ application/views/pengadaan/setting.php
@@ -1,28 +1,31 @@
 <div class="row">
-  <div class="col-md-3">
-    <form action="<?php echo site_url('pengadaan/setting/update') ?>" method="post">
-      <div class="form-group">
+    <div class="col-md-3">
+        <form action="<?php echo site_url('pengadaan/setting/update') ?>" method="post">
+            <div class="form-group">
 
-        <label>Pilih Tahun Anggaran</label>
-        <?php
+                <label>Pilih Tahun Anggaran</label>
+                <?php
         $ta = $this->session->userdata('tahun_anggaran');
         ?>
-        <select name="tahun_anggaran" class="form-control">
-          <option value="2019" <?php echo $ta == '2019' ? 'selected' : '' ?>>2019</option>
-          <option value="2020" <?php echo $ta == '2020' ? 'selected' : '' ?>>2020</option>
-          <option value="2021" <?php echo $ta == '2021' ? 'selected' : '' ?>>2021</option>
-        </select>
+                <select name="tahun_anggaran" class="form-control">
+                    <option value="2019" <?php echo $ta == '2019' ? 'selected' : '' ?>>2019</option>
+                    <option value="2020" <?php echo $ta == '2020' ? 'selected' : '' ?>>2020</option>
+                    <option value="2021" <?php echo $ta == '2021' ? 'selected' : '' ?>>2021</option>
+                    <option value="2022" <?php echo $ta == '2022' ? 'selected' : '' ?>>2022</option>
+                    <option value="2023" <?php echo $ta == '2023' ? 'selected' : '' ?>>2023</option>
+                    <option value="2024" <?php echo $ta == '2024' ? 'selected' : '' ?>>2024</option>
+                </select>
 
-        <!-- CSRF TOKEN -->
-        <?php
+                <!-- CSRF TOKEN -->
+                <?php
         $csrf = array(
           'name' => $this->security->get_csrf_token_name(),
           'hash' => $this->security->get_csrf_hash()
         );
         ?>
-        <input type="hidden" name="<?= $csrf['name']; ?>" value="<?= $csrf['hash']; ?>" />
-      </div>
-      <button type="submit" class="btn btn-primary" name="button">Simpan</button>
-    </form>
-  </div>
+                <input type="hidden" name="<?= $csrf['name']; ?>" value="<?= $csrf['hash']; ?>" />
+            </div>
+            <button type="submit" class="btn btn-primary" name="button">Simpan</button>
+        </form>
+    </div>
 </div>
